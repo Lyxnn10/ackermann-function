a477ebeacbdeb22c657091e1262f06f9e5a7ce66
Lyxnn10 <93597031+Lyxnn10@users.noreply.github.com>
2023-01-17 17:33:05 +0100

    Add files via upload

---
+++ Ackermann.hs
@@ -0,0 +1,11 @@
+Ackermann(m, n)
+{
+  if(m==0)
+    return n+1;
+  else if(m>0 && n==0)
+    return Ackermann(m-1, 1);
+  else if(m>0 && n>0)
+    return Ackermann(m-1, Ackermann(m, n-1));
+  else
+    throw exception;
+}
